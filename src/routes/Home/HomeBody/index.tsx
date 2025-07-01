1ab3a2ae91564d62c745304541ed5273a7942133
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-07-01 17:43:25 -0300

    Starting layout

--- src/routes/Home/HomeBody/index.tsx
+++ src/routes/Home/HomeBody/index.tsx
@@ -1,6 +1,18 @@
+import Button from "../../../components/Button";
+
 export default function HomeBody() {
 
   return (
-    <main></main>
+    <main>
+      <div className="container-home">
+        <div className="title-style mt30">
+          <h2>Desafio Github API</h2>
+          <h3>DevSuperior - Escola de programação</h3>
+        </div>
+        <div className="dflex">
+          <Button text="Começar" />
+        </div>
+      </div>
+    </main>
   );
 }
