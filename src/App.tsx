97b79867f38c8ab520a884bfd32194acc2dc6d22
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-07-01 16:45:26 -0300

    Basic route structure created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import HomeBody from "./routes/Home/HomeBody";
+import SearchProfile from "./routes/Home/SearchProfile";
+
 export default function App() {
 
   return (
-   <h1>Olá mundo</h1>
+    <BrowserRouter> 
+      <Routes>
+        <Route path="/" element={<Home />}>
+        <Route index element={<Navigate to="/home" />} />
+          <Route path="home" element={<HomeBody />} />
+          <Route path="products" element={<SearchProfile />} /> 
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/routes/Home/SearchProfile/index.tsx
@@ -0,0 +1,6 @@
+export default function SearchProfile() {
+
+  return (
+    <main></main>
+  );
+}
